0d81f760baac83ad0e012ba90288b3628e62a5ae
unknown <muhammedarshad898@gmail.com>
2024-08-14 21:00:20 +0530

    msg

---
+++ script.js
@@ -0,0 +1,12 @@
+const getWeather=async()=>{
+    let result=""
+    const city=winp.value
+    const res=await fetch(`http://api.weatherapi.com/v1/current.json?key=ccf0ce9e90bf4881b9a101104241408&q=${city}`)
+    const jsres=await res.json()
+    console.log(jsres)
+    result=`
+    <h3>${jsres.location.name},${jsres.location.region},${jsres.location.country}</h3>
+    <img src=${jsres.current.condition.icon} alt="icon">
+    <h5>${jsres.current.condition.text}</h5>`
+    cnt.innerHTML=result
+}
